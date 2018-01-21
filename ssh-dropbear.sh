71cfe78e997ce8e5f62e2a78b6b06dbe2ab55d62
rasta-team <irfan1037.psa@gmail.com>
2018-01-21 19:01:12 +0800

    Add files via upload

---
+++ ssh-dropbear.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+myip=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
+myint=`ifconfig | grep -B1 "inet addr:$myip" | head -n1 | awk '{print $1}'`;
+
+
+# setting port ssh
+sed -i 's/Port 22/Port  22/g' /etc/ssh/sshd_config
+service ssh restart
+
+
+
+# Install DROPBEAR
+apt-get -y install dropbear
+sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=444/g' /etc/default/dropbear
+echo "/bin/false" » /etc/shells
+service dropbear restart
